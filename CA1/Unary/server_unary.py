10349e419dd601291846aaa243719e8aa8e58d17
sep-81 <sepehr81sepehr@gmail.com>
2024-04-13 23:32:22 +0330

    debug bidirectionl

--- CA1/Unary/server_unary.py
+++ CA1/Unary/server_unary.py
@@ -4,9 +4,14 @@ from datetime import datetime
 import unary_pb2_grpc as pb2_grpc
 import unary_pb2 as pb2
 import sys
-sys.path.append(r"D:\UT\Lessons\Term8\Distributed Systems\Projects")
-
+import os
+# sys.path.append(r"D:\UT\Lessons\Term8\Distributed Systems\Projects")
+# sys.path.append("E:\\ut\\T8\\distributed\\grpc\\")
+# the strucute is: sth/CA1/Unary/unary_script.py
+# run this file directly from its immediate parent folder(unary) 
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 from CA1 import utils 
+
 
 
 
